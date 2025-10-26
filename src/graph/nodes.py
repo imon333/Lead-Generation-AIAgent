a95428631f9a959e039c9b00e3ee62f882e98290
imon333 <imonhosen99@gmail.com>
2025-10-26 11:04:17 +0100

    search_the_web node is done

--- src/graph/nodes.py
+++ src/graph/nodes.py
@@ -1,3 +1,4 @@
+from typing import List
 
 from langchain_openai import ChatOpenAI
 from pydantic import BaseModel
@@ -26,8 +27,39 @@ def refine_search_prompt(state: GraphState):
     response = refiner_chain.invoke({"user_prompt": user_prompt})
 
     # The output of an LLM call is a message object, we get the content
-
     search_query = response.content
+
     print(f"Refined Search Query: {search_query}")
 
     return {"search_query": search_query}
+
+
+def search_the_web(state: GraphState):
+    """
+    Node 2 : Searches the web using the refined search query.
+    """
+    print("---NODE: search_the_web---")
+    search_query = state['search_query']
+
+    # Call our search tool
+    search_results = get_search_results(search_query)
+    print(f"Search Results (snippet): {search_results[:100]}...")
+
+    return{"search_results": search_results}
+
+def extract_lead_data(state: GraphState):
+    """
+    Node 3 : Extracts structured lead data from the search results.
+    """
+    print("---NODE: extract_lead_data---")
+    search_results = state['search_results']
+
+    # This is key: .with_structured_output(lead)
+    # This tells the LLm to format its response *exactly* like out Pydantic 'Lead' class
+    # And to return a *list* of them.
+    class Leads(BaseModel):
+        leads: List[Lead]
+
+    extractor_chain = data_extractor_template | model.with_structured_output(Leads)
+
+
